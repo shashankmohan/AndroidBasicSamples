71cdf78bce8f1b27f109c1abe5c8b592a3c1af8d
developer_shashank <developer.shank@gmail.com>
2018-10-17 22:10:10 +0530

    add another toast to check

--- contentprovidersample/src/main/java/com/sample/shashank/contentprovidersample/MainActivity.java
+++ contentprovidersample/src/main/java/com/sample/shashank/contentprovidersample/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "Check Git", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "checking2", Toast.LENGTH_SHORT).show();
     }
 }
